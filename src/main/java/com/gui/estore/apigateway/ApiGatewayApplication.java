ff3145ddea7700e8ba3c668f7ed9eacceebc5bcb
Guillermo <duche27@hotmail.com>
2022-03-22 20:06:47 +0100

    reordenación

--- src/main/java/com/gui/estore/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/gui/estore/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.gui.store.apigateway;
+package com.gui.estore.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gui/estore/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/gui/estore/apigateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gui.store.apigateway;
+package com.gui.estore.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
